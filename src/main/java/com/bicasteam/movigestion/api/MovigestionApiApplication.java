6a6af23c06928cf8f40ee81aeb607a7654cc3df4
Miguel Huarcaya Chavez <u202116207@upc.edu.pe>
2024-05-29 21:51:19 -0500

    chore: created files

---
+++ src/main/java/com/bicasteam/movigestion/api/MovigestionApiApplication.java
@@ -0,0 +1,13 @@
+package com.bicasteam.movigestion.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovigestionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovigestionApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bicasteam/movigestion/api/MovigestionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bicasteam.movigestion.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovigestionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
