7fcc3a545510eb9d4c951edc8e402ddc5f926e08
Miguel Huarcaya Chavez <u202116207@upc.edu.pe>
2024-06-07 04:04:54 -0500

    fix

--- src/test/java/com/bicasteam/movigestion/platform/profilles/interfaces/rest/transform/ProfileResourceFromEntityAssembler.java
+++ src/test/java/com/bicasteam/movigestion/platform/profilles/interfaces/rest/transform/ProfileResourceFromEntityAssembler.java
@@ -1,5 +1,8 @@
 package com.bicasteam.movigestion.platform.profilles.interfaces.rest.transform;
 
+import com.bicasteam.movigestion.platform.profilles.domain.model.aggregates.Profile;
+import com.bicasteam.movigestion.platform.profilles.interfaces.rest.resources.ProfileResource;
+
 public class ProfileResourceFromEntityAssembler {
     public static ProfileResource toResourceFromEntity(Profile entity) {
         return new ProfileResource(entity.getId(), entity.getFullName(), entity.getEmailAddress(), entity.getStreetAddress());
