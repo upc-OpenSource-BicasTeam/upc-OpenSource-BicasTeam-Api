7f41b5ab09bbde1499dade69dd6f6613acfbcf50
JDiego <JoseHuamani_20@hotmail.com>
2024-06-07 03:02:55 -0500

    feat(domain) add the profile domain

---
+++ src/test/java/com/bicasteam/movigestion/platform/profilles/domain/model/queries/GetProfileByIdQuery.java
@@ -0,0 +1,4 @@
+package com.bicasteam.movigestion.platform.profilles.domain.model.queries;
+
+public record GetProfileByIdQuery(Long profileId) {
+}
--- src/test/java/com/bicasteam/movigestion/platform/profilles/infraestructure/persistence/jpa/repositories/ProfileRepository.java
+++ src/test/java/com/bicasteam/movigestion/platform/profilles/infraestructure/persistence/jpa/repositories/ProfileRepository.java
@@ -1,5 +1,7 @@
 package com.bicasteam.movigestion.platform.profilles.infraestructure.persistence.jpa.repositories;
 
+import com.bicasteam.movigestion.platform.profilles.domain.model.aggregates.Profile;
+import com.bicasteam.movigestion.platform.profilles.domain.model.valueobjects.EmailAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
